9db77893c0b24c830764ff25403650f2ef4916a7
Stefan <stefanhiciu@gmail.com>
2018-05-03 19:56:06 +0200

    updated to take the min value directly from the class

--- second_problem_test.rb
+++ second_problem_test.rb
@@ -6,10 +6,10 @@ class SecondProblemTest < Test::Unit::TestCase
 
   def test_empty_array
     array = {}
-    min = -1
-    minMax = SecondProblem.new(array).get_numbers
-    assert_equal(min, minMax[:second_large])
-    assert_equal(min, minMax[:first_large])
+    max_max = SecondProblem.new(array)
+    minMax = max_max.get_numbers
+    assert_equal(max_max.min_value, minMax[:second_large])
+    assert_equal(max_max.min_value, minMax[:first_large])
   end
 
 
