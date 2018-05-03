07ab9bf4c8c205bd977793d58f84d48086626614
Stefan <stefanhiciu@gmail.com>
2018-05-03 20:17:20 +0200

    added unit test for a array who'se size >2 and it has no duplicates

--- third_problem_test.rb
+++ third_problem_test.rb
@@ -11,4 +11,9 @@ class ThirdProblemTest < Test::Unit::TestCase
     arrayNumbers = [1]
     assert_false(ThirdProblem.new(arrayNumbers).hasDuplicates)
   end
+
+  def test_no_duplicates
+    arrayNumbers = [1,2,3,4,5,6,7]
+    assert_false(ThirdProblem.new(arrayNumbers).hasDuplicates)
+  end
 end
