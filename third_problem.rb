a74e338ffc8b5d968d95c45a82de9652a22b6859
Stefan <stefanhiciu@gmail.com>
2018-05-03 23:05:26 +0200

    extracted to variable the target value

--- third_problem.rb
+++ third_problem.rb
@@ -8,11 +8,12 @@ class ThirdProblem
     @numbers.each {|index|
       absolute_current_index =index.abs-1
       absolut_target_index = @numbers[absolute_current_index].abs-1
-      if @numbers[absolut_target_index] < 0
+      target_value = @numbers[absolut_target_index]
+      if target_value < 0
         found = true
         break
       end
-      @numbers[absolut_target_index] = -@numbers[absolut_target_index]
+      @numbers[absolut_target_index] = -target_value
     }
     found
   end
