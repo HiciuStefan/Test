d95fda0083a801a50f937088b8df7164d99bbf20
Stefan <stefanhiciu@gmail.com>
2018-05-03 19:57:20 +0200

    added failing test for a one number array

--- second_problem_test.rb
+++ second_problem_test.rb
@@ -12,5 +12,13 @@ class SecondProblemTest < Test::Unit::TestCase
     assert_equal(max_max.min_value, minMax[:first_large])
   end
 
+  def test_one_number_array
+    array = [0]
+    max_max = SecondProblem.new(array)
+    minMax = max_max.get_numbers
+    assert_equal(max_max.min_value, minMax[:second_large])
+    assert_equal(0, minMax[:first_large])
+  end
+
 
 end
