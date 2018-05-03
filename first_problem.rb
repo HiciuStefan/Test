4b67ad535f30d2baa1291a207690befd1988adb9
Stefan <stefanhiciu@gmail.com>
2018-05-03 19:31:05 +0200

    updated logic of compute method , no need to check if array is empty. Changed default return to 0 from -1

--- first_problem.rb
+++ first_problem.rb
@@ -7,15 +7,11 @@ class FirstProblem
   end
 
   def compute()
-    sum = -1
-    if @numbers_array.empty? then
-      return sum
-    end
-
+    sum = 0
     @numbers_array.each do |number|
       sum+= number if number.even?
     end
-    sum +1
+    sum
 
   end
 
--- first_problem_test.rb
+++ first_problem_test.rb
@@ -5,7 +5,7 @@ class FirstProblemTest < Test::Unit::TestCase
 
   def test_compute_empty_array_0
     array = Array.new
-    assert_equal(-1,FirstProblem.new(array).compute)
+    assert_equal(0,FirstProblem.new(array).compute)
   end
 
   def test_compute_noEvenNumbersArray_0
