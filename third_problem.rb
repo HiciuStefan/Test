1db60e7560c7c0d22d12fccb8a0d0a54a99ebb7a
Stefan <stefanhiciu@gmail.com>
2018-05-03 20:43:13 +0200

    added logic to handle an array that has no duplicates

--- third_problem.rb
+++ third_problem.rb
@@ -8,8 +8,19 @@ class ThirdProblem
       return false
     end
 
-    true
+    found = false
+    @numbers.each {|index|
+      absolute_current_index =index.abs-1
+      absolut_target_index = @numbers[absolute_current_index].abs-1
+      if @numbers[absolut_target_index] < 0
+        found = true
+        break
+      end
+      @numbers[absolut_target_index] = -@numbers[absolut_target_index]
+    }
+    found
   end
 
-
 end
+
+
