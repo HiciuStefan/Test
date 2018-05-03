06b0eca1407202de2a271a94a408d20f84705ba7
Stefan <stefanhiciu@gmail.com>
2018-05-03 19:55:30 +0200

    added logic for handling an empty array

--- second_problem.rb
+++ second_problem.rb
@@ -1,12 +1,19 @@
 class SecondProblem
 
+  attr_reader :min_value
 
   def initialize(numbers_array)
     @numbers_array = numbers_array
+    Struct.new("MinMax",:second_large, :first_large)
+    @min_value=-1
   end
 
   def get_numbers
-
+    if @numbers_array.empty?
+    then
+      return Struct::MinMax.new(min_value, min_value)
+    end
+    Struct::MinMax.new(0, 0)
   end
 
 end
